bac28df253bf762fddb0788498210bd8c704054c
aliefadha <88047810+aliefadha@users.noreply.github.com>
2025-01-18 13:16:10 +0700

    add metode pembayaran api

--- src/routes/metodePembayaranRoutes.js
+++ src/routes/metodePembayaranRoutes.js
@@ -3,6 +3,10 @@ const MetodePembayaranController = require("../controllers/metodePembayaranContr
 
 const router = express.Router();
 
+router.post("/metode-pembayaran", MetodePembayaranController.create);
 router.get("/metode-pembayaran", MetodePembayaranController.getAll);
+router.get("/metode-pembayaran/:id", MetodePembayaranController.getById);
+router.put("/metode-pembayaran/:id", MetodePembayaranController.update);
+router.delete("/metode-pembayaran/:id", MetodePembayaranController.delete);
 
 module.exports = router;
