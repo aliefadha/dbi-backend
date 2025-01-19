99042efef618fedc2c561522dfff5258d333ce0a
Iqbal Defri Prasetya <iqbaldefriprasetya@gmail.com>
2025-01-19 08:47:57 +0700

    upd Toko

--- src/controllers/tokoController.js
+++ src/controllers/tokoController.js
@@ -1,9 +1,38 @@
 const TokoService = require("../services/tokoService");  
-  
+const bcrypt = require("bcrypt");
+const multer = require("multer");
+const path = require("path");
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../public/toko')); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
 class TokoController {  
   static async create(req, res) {  
     try {  
-      const toko = await TokoService.create(req.body);  
+      const password = req.body.password;  
+      if (password !== req.body.confirmPassword) {  
+        return res.status(400).json({  
+          success: false,  
+          data: null,  
+          message: "password and confirm password not match",  
+        });  
+      }
+      const hashPassword = bcrypt.hashSync(req.body.password, 10);
+      const tokoData = {
+        ...req.body,
+        password: hashPassword,
+        image: req.file.filename
+      }
+      const toko = await TokoService.create(tokoData);  
       res.status(201).json({  
         success: true,  
         data: toko,  
@@ -61,7 +90,28 @@ class TokoController {
   
   static async update(req, res) {  
     try {  
-      const toko = await TokoService.update(req.params.id, req.body);  
+      const existingToko = await TokoService.getById(req.params.id);  
+      if (!existingToko) {  
+        return res.status(404).json({  
+          success: false,  
+          data: null,  
+          message: "not found",  
+        });  
+      }
+      const updatedData = { ...req.body };
+      
+      if (req.file) {  
+        // Delete the old image file  
+        const oldImagePath = path.join(__dirname, "../public/toko", existingToko.image);  
+        fs.unlink(oldImagePath, (err) => {  
+          if (err) {  
+            console.error("Failed to delete old image:", err);  
+          }  
+        });  
+        updatedData.image = req.file.filename;  
+      }
+
+      const toko = await TokoService.update(req.params.id, updatedData);  
       if (!toko) {  
         return res.status(404).json({  
           success: false,  
@@ -108,4 +158,4 @@ class TokoController {
   }  
 }  
   
-module.exports = TokoController;  
+module.exports = {TokoController, upload};  
--- src/routes/tokoRoutes.js
+++ src/routes/tokoRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');  
 const router = express.Router();  
-const TokoController = require('../controllers/tokoController');  
+const {TokoController, upload} = require('../controllers/tokoController');  
 
-router.post('/toko', TokoController.create);  
+router.post('/toko', upload.single("image"),  TokoController.create);  
 router.get('/toko', TokoController.getAll);  
 router.get('/toko/:id', TokoController.getById);  
-router.put('/toko/:id', TokoController.update);  
+router.put('/toko/:id', upload.single("image"), TokoController.update);  
 router.delete('/toko/:id', TokoController.delete);  
   
 module.exports = router;  
