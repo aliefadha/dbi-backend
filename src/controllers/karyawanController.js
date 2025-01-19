a815a7ebae08ac9871da2f040280d57aeaf1cb60
Iqbal Defri Prasetya <iqbaldefriprasetya@gmail.com>
2025-01-19 07:41:41 +0700

    upd img update karyawan

--- src/controllers/karyawanController.js
+++ src/controllers/karyawanController.js
@@ -2,6 +2,7 @@ const KaryawanService = require("../services/karyawanService");
 const bcrypt = require("bcrypt");
 const multer = require("multer");
 const path = require("path");
+const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -80,28 +81,53 @@ class KaryawanController {
         }
     }
 
-    static async update(req, res) {
-        try {
-            const karyawan = await KaryawanService.update(req.params.id, req.body);
-            if (!karyawan) {
-                return res.status(404).json({
-                    success: false,
-                    data: null,
-                    message: "Karyawan not found"
-                });
-            }
-            res.status(200).json({
-                success: true,
-                data: karyawan,
-                message: "Karyawan updated successfully"
-            });
-        } catch (error) {
-            res.status(400).json({
-                success: false,
-                data: null,
-                message: error.message
-            });
-        }
+    static async update(req, res) {  
+        try {  
+            const existingKaryawan = await KaryawanService.getById(req.params.id);  
+            if (!existingKaryawan) {  
+                return res.status(404).json({  
+                    success: false,  
+                    data: null,  
+                    message: "Karyawan not found"  
+                });  
+            }  
+  
+            const updatedData = { ...req.body };  
+  
+            // Check if a new file is uploaded  
+            if (req.file) {  
+                // Delete the old image file  
+                const oldImagePath = path.join(__dirname, "../public/karyawan", existingKaryawan.image);  
+                fs.unlink(oldImagePath, (err) => {  
+                    if (err) {  
+                        console.error("Failed to delete old image:", err);  
+                    }  
+                });  
+  
+                updatedData.image = req.file.filename;  
+            }  
+
+            const karyawan = await KaryawanService.update(req.params.id, updatedData);  
+            if (!karyawan) {  
+                return res.status(404).json({  
+                    success: false,  
+                    data: null,  
+                    message: "Karyawan not found"  
+                });  
+            }  
+  
+            res.status(200).json({  
+                success: true,  
+                data: karyawan,  
+                message: "Karyawan updated successfully"  
+            });  
+        } catch (error) {  
+            res.status(400).json({  
+                success: false,  
+                data: null,  
+                message: error.message  
+            });  
+        }  
     }
 
     static async delete(req, res) {    
