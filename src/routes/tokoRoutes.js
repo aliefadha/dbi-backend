510dca6e27995f2e4855ed64b39d73a979a07d46
aliefadha <88047810+aliefadha@users.noreply.github.com>
2025-03-02 06:46:43 +0700

    add dashboard

--- src/routes/tokoRoutes.js
+++ src/routes/tokoRoutes.js
@@ -4,6 +4,7 @@ const {TokoController, upload} = require('../controllers/tokoController');
 
 router.post('/toko', upload.single("image"),  TokoController.create);  
 router.get('/toko', TokoController.getAll);  
+router.get('/toko/terlaris', TokoController.tokoTerlaris);
 router.get('/toko/:id', TokoController.getById);  
 router.put('/toko/:id', upload.single("image"), TokoController.update);  
 router.delete('/toko/:id', TokoController.delete);  
