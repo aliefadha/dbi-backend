640b9333387f5d7959a88b6f6c5f2f64760fb88e
aliefadha <88047810+aliefadha@users.noreply.github.com>
2025-05-29 23:43:47 +0700

    fixx

--- src/services/biayaGudangService.js
+++ src/services/biayaGudangService.js
@@ -25,19 +25,7 @@ class BiayaGudangService {
         biaya_gudang_id: 1,
         is_deleted: false
       },
-      attributes: ["total", "rata_rata", "total_biaya", "waktu_kerja", "total_modal"],
-      include: [
-        {
-          model: BiayaOperasionalProduksiGudang,
-          as: 'biaya_operasional',
-          attributes: ['nama_biaya', 'total_biaya']
-        },
-        {
-          model: BiayaOperasionalStaffGudang,
-          as: 'biaya_staff',
-          attributes: ['nama_biaya', 'total_biaya']
-        }
-      ]
+      attributes: ["total", "rata_rata", "total_biaya", "waktu_kerja", "total_modal", "persentase"],
     });
   }
 
